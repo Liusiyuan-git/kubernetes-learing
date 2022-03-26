45f8d7e7871a66fd4e77684280beaaaeba90e401
liusiyuan <945212191@qq.com>
2022-03-26 18:40:28 +0800

    week12 homework

---
+++ week12/svc0/go.mod
@@ -0,0 +1,5 @@
+module svc0
+
+go 1.16
+
+require github.com/golang/glog v1.0.0 // indirect
---
+++ week12/svc0/main.go
@@ -0,0 +1,55 @@
+//!/bin/sh
+package main
+
+import (
+	"fmt"
+	"github.com/golang/glog"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", rootHandler)
+	if err := http.ListenAndServe(":80", mux); err != nil {
+		log.Fatalf("start http server failed, error: %s\n", err.Error())
+	}
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	glog.V(4).Info("entering v2 root handler")
+
+	delay := randInt(10, 20)
+	time.Sleep(time.Millisecond * time.Duration(delay))
+	io.WriteString(w, "===================Details of the http request header:============\n")
+	req, err := http.NewRequest("GET", "http://service1/hello", nil)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	lowerCaseHeader := make(http.Header)
+	for key, value := range r.Header {
+		lowerCaseHeader[strings.ToLower(key)] = value
+	}
+	glog.Info("headers:", lowerCaseHeader)
+	req.Header = lowerCaseHeader
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		glog.Info("HTTP get failed with error: ", "error", err)
+	} else {
+		glog.Info("HTTP get succeeded")
+	}
+	if resp != nil {
+		resp.Write(w)
+	}
+	glog.V(4).Infof("Respond in %d ms", delay)
+}
